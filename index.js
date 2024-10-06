535bb67cd513a19d4b52d1a3de394ee0470bdc46
akinloluwami <akinkunmioye42@gmail.com>
2024-10-06 11:09:00 +0100

    Allow cors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { Dub } from "dub";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://www.figma.com",
+  })
+);
 
 export const dub = new Dub({
   token: process.env.DUB_API_KEY,
